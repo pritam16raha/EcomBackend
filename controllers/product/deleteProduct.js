742cf1a9b6b2b60aa93e12906279ed5188e1a2cd
pritam16raha <rahapritam21@yahoo.com>
2025-04-13 19:41:25 +0530

    delete product problem solve

--- controllers/product/deleteProduct.js
+++ controllers/product/deleteProduct.js
@@ -1,27 +1,59 @@
-import { STATUS_CODES } from "http";
-import CustomeErrorHandler from "../../customError/CustomErrorHandler";
+// import { STATUS_CODES } from "http";
+// import CustomeErrorHandler from "../../customError/CustomErrorHandler";
+// import { productModel } from "../../models";
+// import fs from 'fs';
+
+// const deleteProduct = {
+//     async destroy(req, res, next) {
+//         try{
+//             const document = await productModel.findOneAndRemove({ _id: req.params.id });
+//             if(!document){
+//                 return next(new Error("Nothing is there to be deleted"));
+//             }
+    
+//             const imagePath = document.image;
+//             fs.unlink(`${appRoot}/${imagePath}`, (err) => {
+//                 if(err){
+//                     return next(CustomeErrorHandler.serverError("Error from delete product model"));
+//                 }
+//             })
+//         }catch(err){
+//             return next(CustomeErrorHandler.deleteNotPossible())
+//         }
+//         res.status(200);
+//     }
+// }
+
+// export default deleteProduct;
+
 import { productModel } from "../../models";
-import fs from 'fs';
+import fs from "fs";
+import path from "path";
+import CustomeErrorHandler from "../../customError/CustomErrorHandler";
 
 const deleteProduct = {
-    async destroy(req, res, next) {
-        try{
-            const document = await productModel.findOneAndRemove({ _id: req.params.id });
-            if(!document){
-                return next(new Error("Nothing is there to be deleted"));
-            }
-    
-            const imagePath = document.image;
-            fs.unlink(`${appRoot}/${imagePath}`, (err) => {
-                if(err){
-                    return next(CustomeErrorHandler.serverError("Error from delete product model"));
-                }
-            })
-        }catch(err){
-            return next(CustomeErrorHandler.deleteNotPossible())
+  async destroy(req, res, next) {
+    try {
+      const document = await productModel.findOneAndRemove({
+        _id: req.params.id,
+      });
+      if (!document) {
+        return next(new Error("Nothing is there to be deleted"));
+      }
+
+      const imagePath = document.image;
+      const fullPath = path.resolve() + "/" + imagePath;
+      fs.unlink(fullPath, (err) => {
+        if (err) {
+          console.error("Image deletion failed:", err.message);
         }
-        res.status(200);
+      });
+
+      return res.status(200).json({ message: "Product deleted successfully" });
+    } catch (err) {
+      return next(CustomeErrorHandler.deleteNotPossible());
     }
-}
+  },
+};
 
 export default deleteProduct;
--- server.js
+++ server.js
@@ -1,33 +1,38 @@
-import express from 'express';
+import express from "express";
 
 const app = express();
 
-import {APP_KEY, ECOMDB, PORT} from './config';
+import { APP_KEY, ECOMDB, PORT } from "./config";
 
-import router from './routes';
+import router from "./routes";
 
-import errorHandler from './middleware/errorHandler';
-import mongoose from 'mongoose';
-import CustomeErrorHandler from './customError/CustomErrorHandler';
-import path from 'path';
+import errorHandler from "./middleware/errorHandler";
+import mongoose from "mongoose";
+import CustomeErrorHandler from "./customError/CustomErrorHandler";
+import path from "path";
 import cors from "cors";
 
-const port = process.env.PORT || APP_KEY
+const port = process.env.PORT || APP_KEY;
 
 global.appRoot = path.resolve(__dirname); //appRoot is used in product->productController.js->line number 33
 
-app.use(express.urlencoded({ extended: false })) //to accecpt multipart form data, we use this line
+app.use(express.urlencoded({ extended: false })); //to accecpt multipart form data, we use this line
 
 app.use(cors());
 
 app.use(express.json()); //to accecpt json form data, we use this line
 
-app.use('/ecom',router);
+app.use("/ecom", router);
 
 app.use(errorHandler);
 
 app.listen(port, () => console.log(`listening on port ${port}`));
 
-mongoose.connect(ECOMDB).then(() => console.log("DB Connected! now get lost")).catch((err) => {
-    CustomeErrorHandler.DbError("you can not connect database with backend, please die in peace!")
-})
+mongoose
+  .connect(ECOMDB)
+  .then(() => console.log("DB Connected! now get lost"))
+  .catch((err) => {
+    CustomeErrorHandler.DbError(
+      "you can not connect database with backend, please die in peace!"
+    );
+  });
